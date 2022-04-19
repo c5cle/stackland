c4ec0036c1c76dc9a399e7af591ffe90e2de1d7f
c5cle <182622227804@163.com>
2022-04-19 12:38:15 +0800

    improvement: progress use animejs to run

--- src/components/CardProgress.vue
+++ src/components/CardProgress.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import anime from 'animejs'
 
 const props = defineProps({
   progress: {
@@ -12,12 +13,7 @@ const props = defineProps({
 
 const el = ref<HTMLInputElement | null>(null)
 const show = ref(false)
-const id = ref(null)
 const progress = ref(props.progress)
-
-const getWidth = () => {
-  return progress.value / props.progress * 100
-}
 
 const setShow = (state: boolean) => {
   show.value = state
@@ -28,27 +24,35 @@ onMounted(() => {
   setShow(false)
   if (props.progress > 0) {
     setShow(true)
-    id.value = setInterval(() => {
-      progress.value = progress.value - 1
-      if (progress.value < 0) {
-        clearInterval(id)
-        setShow(false)
-      }
-    }, 1000)
+
+    const change = {
+      progress: progress.value,
+    }
+
+    anime({
+      targets: change,
+      progress: 0,
+      easing: 'easeInOutExpo',
+      duration: progress.value * 1000,
+      update() {
+        el.value.style.width = `${change.progress / (progress.value) * 100}%`
+        if (change.progress === 0)
+          setShow(false)
+      },
+    })
   }
 })
 
 onBeforeUnmount(() => {
   setShow(false)
-  clearInterval(id)
 })
 
 </script>
 
 <template>
-  <div v-if="show" ref="el" class="progress" h="3.5" bg-gray-100 dark:bg-gray-900 border-4 absolute>
+  <div v-show="show" class="progress -top-6" left="-5%" w="110%" h="3.5" bg-gray-100 dark:bg-gray-900 border-4 absolute>
     <div
-      :style="'width:' + (getWidth()) + '%'"
+      ref="el"
       transition-all ease-in-out
       h-full bg-gray-900 dark:bg-gray-100
     />
@@ -56,9 +60,4 @@ onBeforeUnmount(() => {
 </template>
 
 <style scope>
-.progress {
-  left: -5%;
-  top: -23px;
-  width: 110%;
-}
 </style>
