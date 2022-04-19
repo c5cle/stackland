e6b11ca0ebdd7c8e99c886aa3a2e462947713b14
c5cle <182622227804@163.com>
2022-04-19 17:47:47 +0800

    style: 404.vue

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-const router = useRouter()
-const { t } = useI18n()
 </script>
 
 <template>
@@ -9,10 +7,5 @@ const { t } = useI18n()
       <div i-carbon-warning inline-block />
     </div>
     <RouterView />
-    <div>
-      <button btn text-sm m="3 t8" @click="router.back()">
-        {{ t('button.back') }}
-      </button>
-    </div>
   </main>
 </template>
